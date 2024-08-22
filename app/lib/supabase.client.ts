4c6a95bdf59db76d887ae7a01929473f6fcb4fbc
hiro08gh <hiro08gh@gmail.com>
2024-08-22 21:50:07 +0900

    chore: replace createBrowerClient

--- app/lib/supabase.client.ts
+++ app/lib/supabase.client.ts
@@ -1,10 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
-	throw new Error("環境変数が設定されていません");
-}
+export const createClient = () => {
+	if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+		throw new Error("環境変数が設定されていません");
+	}
 
-export const supabase = createClient(
-	process.env.SUPABASE_URL,
-	process.env.SUPABASE_ANON_KEY,
-);
+	return createBrowserClient(
+		process.env.SUPABASE_URL,
+		process.env.SUPABASE_ANON_KEY,
+	);
+};
